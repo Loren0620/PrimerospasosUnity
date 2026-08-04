b33c773432787e550d0bd7489fb62c62d274ba8d
Loren0620 <lorennicholson9@gmail.com>
2026-08-04 11:25:48 -0500

    variables

--- MyUnity/Assets/Main/Scripts/PlayerMovement.cs
+++ MyUnity/Assets/Main/Scripts/PlayerMovement.cs
@@ -2,29 +2,34 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement: MonoBehaviour
+public class PlayerMovement : MonoBehaviour
 {
     //Variables
-    [SerializeField] private int _edad = 19;
-    private float _velocidad = 15.5f;
-    private string nombre = "Loren";
-    private bool isJumping = false;
+ 
+    public float altura = 1.70f;
+    public int edad = 19;
+    public string nombre = "Loren";
+    public bool puedeVotar = true;
 
-    public float fuerza = 500f;
 
-    [SerializeField] private Rigidbody2D _rigidbody2;
+    public GameObject gameObject;
+    public Rigidbody2D rigidbody2D;
+    public Collider2D collider2D;
+    public SpriteRenderer spriteRenderer;
 
-    // Start is called before the first frame update
-    void Start()
+    public Transform transform;
+    private void Start()
     {
-        _rigidbody2.AddForce(Vector2.right * fuerza);
-        Debug.Log("Mi edad es: " + _edad);
-    }   
+        Debug.Log("Hola: " + nombre + " tu edad es: " + edad + " tu altura es: " + altura);
+        rigidbody2D.simulated = false;
+        spriteRenderer.color = Color.white;
+        transform.position = new Vector3(10, 0, 0);
+    }
+    
+    private void update ()
 
-
-    // Update is called once per frame
-    void Update()
     {
-        Debug.Log("UpDate");
+
     }
 }
+
