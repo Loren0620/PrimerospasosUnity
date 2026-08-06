77591f5a83064ac2b6a8b9bd9735e48e4c4648a5
Loren0620 <lorennicholson9@gmail.com>
2026-08-06 11:34:18 -0500

    Intro a if

--- MyUnity/Assets/Main/Scripts/PlayerMovement.cs
+++ MyUnity/Assets/Main/Scripts/PlayerMovement.cs
@@ -1,35 +1,32 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
     //Variables
- 
-    public float altura = 1.70f;
-    public int edad = 19;
-    public string nombre = "Loren";
-    public bool puedeVotar = true;
 
 
-    public GameObject gameObject;
-    public Rigidbody2D rigidbody2D;
-    public Collider2D collider2D;
-    public SpriteRenderer spriteRenderer;
+    [SerializeField] private float _force = 5f;
+    [SerializeField] private float _speed = 5f;
+    [SerializeField] private Rigidbody2D _rigidbody2D;
 
-    public Transform transform;
     private void Start()
     {
-        Debug.Log("Hola: " + nombre + " tu edad es: " + edad + " tu altura es: " + altura);
-        rigidbody2D.simulated = false;
-        spriteRenderer.color = Color.white;
-        transform.position = new Vector3(10, 0, 0);
+        _rigidbody2D = GetComponent<Rigidbody2D>();
     }
-    
-    private void update ()
+
+    private void Update()
 
     {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            _rigidbody2D.AddForce(Vector2.up * _force);
 
+            
+        }
+       
+        _rigidbody2D.velocity = Vector2.right * _speed * Time.deltaTime;
+        
+          
     }
 }
 
